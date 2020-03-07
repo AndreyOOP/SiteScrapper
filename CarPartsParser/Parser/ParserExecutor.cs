f854040968fd5466f11da7242a0e09deca2144fd
AndreyOOP <andrew_yudaichev@i.ua>
2020-03-07 21:33:28 +0200

    T2 | Add ParserExecutor

---
+++ CarPartsParser/Parser/ParserExecutor.cs
@@ -0,0 +1,30 @@
+﻿using CarPartsParser.Abstraction.Factories;
+using CarPartsParser.Abstraction.Models;
+using CarPartsParser.Models;
+using System.Collections.Generic;
+
+namespace CarPartsParser.SiteParsers.RootParser
+{
+    public class ParserExecutor
+    {
+        private IWebSiteParsersFactory factory;
+
+        public ParserExecutor(IWebSiteParsersFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public IEnumerable<ParserExecutorResult> Parse(IWorkUnitModel id)
+        {
+            var result = new List<ParserExecutorResult>();
+
+            foreach (var parser in factory.GetAll())
+            {
+                var siteResult = parser.Parse(id);
+                result.Add((ParserExecutorResult)siteResult);
+            }
+
+            return result;
+        }
+    }
+}
