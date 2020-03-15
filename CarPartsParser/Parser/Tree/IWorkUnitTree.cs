65a94eab26cf01e49e99140e98ed2040e442fab5
AndreyOOP <andrew_yudaichev@i.ua>
2020-03-15 15:12:26 +0200

    T4 | Add WorkUnitTree  - Class incapsulates logic of execution path creation & accessing to the next node in the execution path. As well contains helper method for displaying current tree of work units

---
+++ CarPartsParser/Parser/Tree/IWorkUnitTree.cs
@@ -0,0 +1,17 @@
+﻿using CarPartsParser.Abstraction.Models;
+using CarPartsParser.SiteParsers.Abstraction.WorkUnits;
+using System;
+using System.Collections.Generic;
+
+namespace CarPartsParser.Parser.Tree
+{
+    public interface IWorkUnitTree
+    {
+        IWorkUnit Unit { get; }
+
+        Dictionary<ExecutionPath, WorkUnitTree> AddConditionalNodes(Func<IWorkUnitModel, IWorkUnitModel, ExecutionPath> getPath, Dictionary<ExecutionPath, WorkUnitTree> pathToTreeNode);
+        WorkUnitTree AddNextNode(WorkUnitTree node);
+        bool IsLastNode();
+        WorkUnitTree NextNode(IWorkUnitModel input, IWorkUnitModel output);
+    }
+}
