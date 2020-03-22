971c3f7575981063e3f4304cf670a9c1cbe48e7b
AndreyOOP <andrew_yudaichev@i.ua>
2020-03-23 01:02:09 +0200

    T5_2 | Add SiteParsingHelper  - here moved WebSiteParser related abstractions & implementation will be nuget package

---
+++ SiteParsingHelper/Tree/ExecutionPath.cs
@@ -0,0 +1,7 @@
+﻿namespace SiteParsingHelper.Tree
+{
+    public enum ExecutionPath
+    {
+        Path1, Path2, Path3, Path4, Path5
+    }
+}
---
+++ SiteParsingHelper/WebSiteParser.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using SiteParsingHelper.Abstraction;
+using SiteParsingHelper.Tree;
+using System;
+
+namespace SiteParsingHelper
+{
+    public class WebSiteParser<TUnit, TOut> : IWebSiteParser<TOut> 
+        where TUnit : IWorkUnit 
+        where TOut : ParserExecutorResultBase, new()
+    {
+        private WorkUnitTree tree;
+
+        public WebSiteParser(WorkUnitTree tree)
+        {
+            this.tree = tree;
+        }
+
+        public TOut Parse(IWorkUnitModel input)
+        {
+            var node = tree;
+            var model = input;
+            var siteResult = (ParserExecutorResultBase)new TOut();
+
+            for(;;)
+            {
+                if (siteResult.Exception != null)
+                {
+                    break;
+                }
+
+                var modelBeforeUpdate = model;
+
+                try
+                {
+                    siteResult.ExecutionPath += $"{node.Unit.GetType().Name} > ";
+
+                    model = node.Unit.Execute(model, ref siteResult);
+                    
+                    if (node.IsLastNode())
+                        break;
+                    
+                    node = node.NextNode(modelBeforeUpdate, model);
+                }
+                catch (Exception ex)
+                {
+                    siteResult.Exception = JsonConvert.SerializeObject(new
+                    {
+                        UnitName = node.Unit.GetType().Name,
+                        Message = ex.Message,
+                        Path = siteResult.ExecutionPath
+                    });
+                }
+            }
+
+            return (TOut)siteResult;
+        }
+    }
+}
