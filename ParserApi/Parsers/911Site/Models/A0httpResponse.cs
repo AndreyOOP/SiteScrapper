0020a3074f3025973b7a54281eba4dcf8c5bc410
AndreyOOP <andrew_yudaichev@i.ua>
2020-03-30 23:51:12 +0300

    TI2 | Add A0 WorkUnit  - add in & out models

---
+++ ParserApi/Parsers/911Site/Models/A0httpResponse.cs
@@ -0,0 +1,9 @@
+﻿using SiteParsingHelper.Abstraction;
+
+namespace ParserApi.Parsers.Site911Parser.Models
+{
+    public class A0httpResponse : IWorkUnitModel
+    {
+        public string Html { get; set; }
+    }
+}
---
+++ ParserApi/Parsers/911Site/Models/Input.cs
@@ -0,0 +1,9 @@
+﻿using SiteParsingHelper.Abstraction;
+
+namespace ParserApi.Parsers.Site911Parser.WorkUnits.Models
+{
+    public class Input : IWorkUnitModel
+    {
+        public string Id { get; set; }
+    }
+}
