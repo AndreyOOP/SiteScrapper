ff571a39acff2b90e1844dcc03e63e7f819f13fc
AndreyOOP <andrew_yudaichev@i.ua>
2020-03-30 23:53:07 +0300

    TI2 | Add Site911Parser  - add site parser result model  - web config updated

---
+++ ParserApi/Parsers/911Site/Models/Site911Result.cs
@@ -0,0 +1,10 @@
+﻿using SiteParsingHelper;
+
+namespace ParserApi.Parsers.Site911Parser
+{
+    public class Site911Result : ParserExecutorResultBase
+    {
+        public string ModelName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
