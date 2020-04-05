285bdff7692bae79132737d45ab68606f0cb0842
AndreyOOP <andrew_yudaichev@i.ua>
2020-04-06 00:21:08 +0300

    TI2 | Add C0httpRequestPartDetails  - add unit test

---
+++ ParserApi.Tests/RealDataAcceptanceTests/911Site/WorkUnits/C0httpRequestPartDetailsTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParserApi.Parsers._911Site.Models;
+using ParserApi.Parsers._911Site.WorkUnits;
+using ParserApi.Parsers.Site911Parser;
+using SiteParsingHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserApi.Tests.RealDataAcceptanceTests._911Site.WorkUnits
+{
+    [TestClass]
+    public class C0httpRequestPartDetailsTest
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var httpClient = new HttpClient();
+
+            var workUnit = new C0httpRequestPartDetails(httpClient);
+            var inModel = new B0parseHtmlResult { QueryString = "svc=1&q=%3D%3Dqn2GtoXydrn1dDZvwDXvMC" };
+            var result = (ParserExecutorResultBase)new Site911Result();
+
+            var c0Result = (C0httpResponse)workUnit.Execute(inModel, ref result);
+
+            Assert.IsNull(result.Exception);
+            Assert.IsTrue(c0Result.Html.Length > 0);
+        }
+    }
+}
---
+++ ParserApi/Parsers/911Site/Models/C0httpResponse.cs
@@ -0,0 +1,9 @@
+﻿using SiteParsingHelper.Abstraction;
+
+namespace ParserApi.Parsers._911Site.Models
+{
+    public class C0httpResponse : IWorkUnitModel
+    {
+        public string Html { get; set; }
+    }
+}
