a22aadc05ccfce1e70ecf721f7133600d1a334d0
AndreyOOP <andrew_yudaichev@i.ua>
2020-04-06 00:05:21 +0300

    TI2 | Update API Controller  - remove unnecessary tests

--- ParserApi.Tests/ControllersTemp/ValuesControllerTest.cs
+++ ParserApi.Tests/ControllersTemp/ValuesControllerTest.cs
@@ -1,33 +1,20 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ParserApi.Controllers;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ParserApi.Tests.Controllers
 {
     [TestClass]
     public class ValuesControllerTest
     {
+        // ToDo:
         [TestMethod]
         public void Get()
         {
-            ValuesController controller = new ValuesController();
+            //ValuesController controller = new ValuesController();
 
-            IEnumerable<string> result = controller.Get();
+            //IEnumerable<string> result = controller.Get();
 
-            Assert.AreEqual("value1", result.ElementAt(0));
-            Assert.AreEqual("value2", result.ElementAt(1));
-        }
-
-        [TestMethod]
-        public void GetById()
-        {
-            ValuesController controller = new ValuesController();
-
-            TestModel result = controller.Get(5);
-
-            Assert.AreEqual(1, result.Id);
-            Assert.AreEqual(2, result.Id2);
+            //Assert.AreEqual("value1", result.ElementAt(0));
+            //Assert.AreEqual("value2", result.ElementAt(1));
         }
     }
 }
