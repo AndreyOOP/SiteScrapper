e03f1c52a0e13c77e7090de5f5bd929eae149f25
AndreyOOP <andrew_yudaichev@i.ua>
2020-06-23 22:09:25 +0300

    PC-3 | Add ExecutionPath Unit Tests  - make ExecutionPath visible to test project

--- SiteParsingHelper/ParserCore/ExecutionPath.cs
+++ SiteParsingHelper/ParserCore/ExecutionPath.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("ParserCoreProjectTests")]
 namespace ParserCoreProject.ParserCore
 {
     internal class ExecutionPath : IExecutionPath
