76fa61e5540c7e558e88bfc9d9613bf05e8b9332
AndreyOOP <andrew_yudaichev@i.ua>
2020-06-24 22:03:57 +0300

    PC-6 | Add WorkerSharedServices  - it is container for services shared among workers  - add interface & implementation

---
+++ SiteParsingHelper/Abstraction/IWorkerSharedServices.cs
@@ -0,0 +1,11 @@
+﻿namespace ParserCoreProject.Abstraction
+{
+    public interface IWorkerSharedServices<TFirstIn, TFirstOut, TResult>
+    {
+        IWorkersContainer<TFirstIn, TFirstOut> WorkersContainer { get; }
+
+        IWorkerPreprocessorsContainer WorkersPreprocessorsContainer { get; }
+
+        TResult Result { get; }
+    }
+}
