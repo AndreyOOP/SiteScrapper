be69c2abfbb1946ee92f41afe8c7d3e215b6840d
AndreyOOP <andrew_yudaichev@i.ua>
2020-06-30 00:16:09 +0300

    PC-8 | Add IEdgeWorkers - Setup Start & End Workers

---
+++ SiteParsingHelper/Abstraction/IEdgeWorkers.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ParserCoreProject.Abstraction
+{
+    public interface IEdgeWorkers
+    {
+        IInOutKey FirstWorker { get; }
+
+        IEnumerable<IInOutKey> LastWorkers { get; }
+    }
+}
