1c775d53be8f2f92f9f4fa26a97b416c5f94dc0f
AndreyOOP <andrew_yudaichev@i.ua>
2020-06-29 15:12:43 +0300

    PI-0 | Update Controller to New Parser

--- ParserApi/Controllers/ValuesController.cs
+++ ParserApi/Controllers/ValuesController.cs
@@ -1,6 +1,9 @@
 ﻿using HtmlAgilityPack;
+using ParserApi.Parsers.Site911Demo.Models;
+using ParserApi.Parsers.Site911Demo.WorkUnits;
 using ParserApi.Parsers.Site911EventBusPrototype.Models;
 using ParserApi.Parsers.Site911EventBusPrototype.WorkUnits;
+using ParserCoreProject.ParserCore;
 //using ParserCoreProject.ParserCore;
 using System.Net.Http;
 using System.Web.Http;
@@ -25,6 +28,26 @@ namespace ParserApi.Controllers
 
             //return parser.Result;
             return null;
+        }
+
+        [HttpGet]
+        [Route("api/core/{id}")]
+        public Result5 GetResultParserCore([FromUri]string id)
+        {
+            var httpClient = new HttpClient();
+
+            var workersContainer = new WorkersContainer<In1, Html2>();
+            var workerPreprocessorsContainer = new WorkerPreprocessorsContainer();
+            var workerSharedServices = new WorkerSharedServices<In1, Html2, Result5>(workersContainer, workerPreprocessorsContainer, new Result5());
+
+            workersContainer.Add(new Step1(workerSharedServices, httpClient));
+            workersContainer.Add(new Step2(workerSharedServices, new HtmlDocument()));
+            workersContainer.Add(new Step3(workerSharedServices, httpClient));
+            workersContainer.Add(new Step4(workerSharedServices, new HtmlDocument()));
+
+            workersContainer.GetFirst().ParseAndExecuteNext(new In1 { Id = id });
+
+            return workerSharedServices.Result;
         }
     }
 }
