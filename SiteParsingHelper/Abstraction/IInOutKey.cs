f4a222324449adf138143e6c785f09c1346dcfef
AndreyOOP <andrew_yudaichev@i.ua>
2020-06-30 00:15:22 +0300

    PC-8 | Add InOutKey to Store Workers  - add unit tests

---
+++ SiteParsingHelper/Abstraction/IInOutKey.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ParserCoreProject.Abstraction
+{
+    public interface IInOutKey
+    {
+        Type InType { get; }
+        Type OutType { get; }
+    }
+}
---
+++ SiteParsingHelperTests/UnitTests/InOutKeyTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParserCoreProject.ParserCore;
+
+namespace ParserCoreProjectTests.UnitTests.InOutKeyTests
+{
+    class A { }
+    class B { }
+
+    [TestClass]
+    public class InOutKeyTests
+    {
+        [TestMethod]
+        public void Equals_SameKeys_True()
+        {
+            var key1 = new InOutKey<A, B>();
+            var key2 = new InOutKey<A, B>();
+
+            Assert.IsTrue(key1.Equals(key2));
+            Assert.IsTrue(key1 == key2);
+        }
+
+        [TestMethod]
+        public void Equals_SameKeys_True2()
+        {
+            var key1 = new InOutKey<A, B>();
+            var key2 = new InOutKey<A, B>();
+            var key3 = new InOutKey<A, B>();
+
+            Assert.IsTrue(key1.Equals(key2));
+            Assert.IsTrue(key2.Equals(key3));
+            Assert.IsTrue(key1 == key2);
+            Assert.IsTrue(key2 == key3);
+        }
+
+        [TestMethod]
+        public void Equals_DifferentKeys_False()
+        {
+            var key1 = new InOutKey<A, B>();
+            var key2 = new InOutKey<B, A>();
+
+            Assert.IsFalse(key1.Equals(key2));
+            Assert.IsFalse(key1 == key2);
+        }
+
+        [TestMethod]
+        public void Equals_OneKeyIsNull_False()
+        {
+            var key1 = new InOutKey<A, B>();
+            object key2 = null;
+
+            Assert.IsFalse(key1.Equals(key2));
+            Assert.IsFalse(key1 == key2);
+        }
+    }
+}
