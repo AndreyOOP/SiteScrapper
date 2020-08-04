9313131075d79a9a317360f264d49444413d2b98
AndreyOOP <andrew_yudaichev@i.ua>
2020-08-04 22:52:15 +0300

    PCN-2 | Add Last Property Implementation  - add unit tests

--- ParserCore/WorkersContainer.cs
+++ ParserCore/WorkersContainer.cs
@@ -1,46 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ParserCore
 {
     public class WorkersContainer : IWorkersContainer
     {
+        private List<IInOutKey> last = new List<IInOutKey>();
         private Dictionary<IInOutKey, object> workers = new Dictionary<IInOutKey, object>();
 
-        public WorkersContainer()
-        {
-        }
-
-        public WorkersContainer(Dictionary<IInOutKey, object> workers) // just for quick test
+        /// <param name="workers">Represents workers relations</param>
+        public WorkersContainer(Dictionary<IInOutKey, object> workers)
         {
             this.workers = workers;
+            last = GetLastWorkers();
         }
 
-        public void Register(IEnumerable<KeyToWorker> workers)
+        public void Register(IEnumerable<object> workers)
         {
+            // type check
+
+            // build graph
+
+            last = GetLastWorkers();
+
             throw new NotImplementedException();
         }
 
-        public IEnumerable<IInOutKey> Last {
-            get 
-            {
-                var lst = new List<IInOutKey>();
-                foreach(var a in workers.Keys)
-                {
-                    if(workers.Keys.All(k => k.InType != a.OutType))
-                        lst.Add(a);
-                }
-                return lst;
-            }
-        }
+        /// <inheritdoc/>
+        public IEnumerable<IInOutKey> Last => last;
 
         public IEnumerable<KeyToWorker> Get(Type typeIn)
         {
             return workers.Where(w => w.Key.InType == typeIn)
                           .Select(x => new KeyToWorker { Key = x.Key, Worker = x.Value });
+        }
+
+        private List<IInOutKey> GetLastWorkers()
+        {
+            Func<Type, bool> LastType = 
+                (outType) => workers.Keys.All(key => key.InType != outType);
+
+            return workers.Keys.Where(key => LastType(key.OutType))
+                               .ToList();
         }
     }
 }
---
+++ ParserCoreTests/UnitTests/WorkersContainerTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParserCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTests.WorkersContainerUnitTest
+{
+    #region Test Classes
+    class A { }
+    class B { }
+    class C { }
+    class D { }
+    class E { }
+    class F { }
+    class G { }
+    class J { }
+    #endregion
+
+    [TestClass]
+    public class WorkersContainerTests
+    {
+        #region InOut keys
+        InOutKey<A, B> ab = new InOutKey<A, B>();
+        InOutKey<B, C> bc = new InOutKey<B, C>();
+        InOutKey<C, D> cd = new InOutKey<C, D>();
+        InOutKey<C, E> ce = new InOutKey<C, E>();
+        InOutKey<E, F> ef = new InOutKey<E, F>();
+        InOutKey<D, G> dg = new InOutKey<D, G>();
+        InOutKey<C, J> cj = new InOutKey<C, J>();
+        #endregion
+
+        [TestMethod]
+        public void Last_SingleBranch_Bc()
+        {
+            // AB-BC
+            var graph = new Dictionary<IInOutKey, object>
+            {
+                [ab] = null, [bc] = null
+            };
+            var container = new WorkersContainer(graph);
+
+            CollectionAssert.AreEqual(new[] { bc }, container.Last.ToArray());
+        }
+
+        [TestMethod]
+        public void Last_TwoSameBranches_CdCe()
+        {
+            // AB-BC-CD
+            //     \CE
+            var graph = new Dictionary<IInOutKey, object>
+            {
+                [ab] = null, [bc] = null, [cd] = null, [ce] = null
+            };
+            var container = new WorkersContainer(graph);
+
+            CollectionAssert.AreEqual(new object[] { cd, ce }, container.Last.ToArray());
+        }
+
+        [TestMethod]
+        public void Last_TwoDifferentBranches_CdEf()
+        {
+            // AB-BC-CD
+            //     \CE-EF
+            var graph = new Dictionary<IInOutKey, object>
+            {
+                [ab] = null, [bc] = null, [cd] = null, [ce] = null, [ef] = null
+            };
+            var container = new WorkersContainer(graph);
+
+            CollectionAssert.AreEqual(new object[] { cd, ef }, container.Last.ToArray());
+        }
+
+        [TestMethod]
+        public void Last_ThreeDifferentBranches_DgEfCj()
+        {
+            // AB-BC-CD-DG
+            //     \CE-EF
+            //      \CJ
+            var graph = new Dictionary<IInOutKey, object>
+            {
+                [ab] = null, [bc] = null, [cd] = null, [dg] = null, [ce] = null, [ef] = null, [cj] = null
+            };
+            var container = new WorkersContainer(graph);
+
+            CollectionAssert.AreEqual(new object[] { dg, ef, cj }, container.Last.ToArray());
+        }
+    }
+}
