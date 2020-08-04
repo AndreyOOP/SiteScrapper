f1c2f6c5fd502413ea02b4edde11ead0091aa072
AndreyOOP <andrew_yudaichev@i.ua>
2020-08-04 21:28:24 +0300

    PCN-2 | Move IInOutKey to Abstraction Folder  - add documentation

---
+++ ParserCore/Abstraction/IInOutKey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ParserCore
+{
+    /// <summary>
+    /// Key identifies Worker<TIn, TOut>
+    /// Each worker receives Output model based on Input model, so worker unique defined by Input & Output types combination
+    /// </summary>
+    public interface IInOutKey
+    {
+        /// <summary>
+        /// Type of worker input model
+        /// </summary>
+        Type InType { get; }
+
+        /// <summary>
+        /// Type of worker output model
+        /// </summary>
+        Type OutType { get; }
+    }
+}
