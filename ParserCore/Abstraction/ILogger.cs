dd310c0ad4bcfb66d6dfa333f2a546602275d61a
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-01 00:14:47 +0300

    PCN-5 | Add InMemoryWorkerLogger Implementation of ILogger

---
+++ ParserCore/Abstraction/ILogger.cs
@@ -0,0 +1,11 @@
+﻿namespace ParserCore.Abstraction
+{
+    public interface ILogger<TRecord>
+    {
+        /// <summary>
+        /// Logs TRecord data
+        /// </summary>
+        /// <param name="record">Log message, could be more complex than just a string</param>
+        void Log(TRecord record);
+    }
+}
