0fb3af7240b4848aab92ddb31c82b71d80a53d28
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-01 00:19:52 +0300

    PCN-5 | Add DefaultSettings - Implementation of WorkerLogSettings

---
+++ ParserApi/Parsers/Site911ParserCore/Models/DefaultSettings.cs
@@ -0,0 +1,12 @@
+﻿using ParserCore.Abstraction;
+
+namespace ParserApi.Parsers.Site911ParserCore.Models
+{
+    public class DefaultSettings : WorkerLogSettings
+    {
+        public override bool ShowStackTrace => true;
+        public override bool ShowExceptionMessage => true;
+        public override bool AddInModel => true;
+        public override bool LogOnlyParseMethod => true;
+    }
+}
