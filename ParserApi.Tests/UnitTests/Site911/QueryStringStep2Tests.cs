b3bb3f39a8c23cf9e40f5b27f301b3eee4294789
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-04 00:49:41 +0300

    PI-2 | Add QueryStringStep2 Model  - add unit tests

---
+++ ParserApi.Tests/UnitTests/Site911/QueryStringStep2Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParserApi.Parsers.Site911.Models;
+
+namespace ParserApi.UnitTests
+{
+    [TestClass]
+    public class QueryStringStep2Tests
+    {
+        [TestMethod]
+        [DataRow("svc=1&q=%3D%3Dqn2GtoXydrn1dDZvwDXvMC")]
+        public void Value_FullQueryString_Ok(string fullQueryString)
+        {
+            var queryString = new QueryStringStep2(fullQueryString);
+
+            Assert.AreEqual("==qn2GtoXydrn1dDZvwDXvMC", queryString.Value);
+        }
+    }
+}
