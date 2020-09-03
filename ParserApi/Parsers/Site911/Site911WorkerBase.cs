fd5f3fab2f0f4994abf7a2a20d5be7e2d457c07c
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-03 22:04:09 +0300

    PI-2 | Implement Site911WorkerBase

---
+++ ParserApi/Parsers/Site911/Site911WorkerBase.cs
@@ -0,0 +1,14 @@
+﻿using ParserCore;
+using System;
+
+namespace ParserApi.Parsers.Site911
+{
+    public abstract class Site911WorkerBase<TIn, TOut> : IWorker<TIn, TOut>
+    {
+        protected readonly Uri Site911Base = new Uri("https://911auto.com.ua/");
+
+        public bool IsExecutable(TIn model) => true;
+
+        public abstract TOut Parse(TIn model);
+    }
+}
