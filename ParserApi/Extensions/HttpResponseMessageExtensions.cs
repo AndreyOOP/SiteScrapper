5b04219fd482e28c621aff1829883fd0310bc8db
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-03 21:53:45 +0300

    PI-2 | Add GetContent Etension to HttpResponseMessage  - add HttpStatusCodeNotOk message

---
+++ ParserApi/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web;
+
+namespace ParserApi.Extensions
+{
+    public static class HttpResponseMessageExtensions
+    {
+        /// <summary>
+        /// Extracts string content from <see cref="HttpResponseMessage"/>
+        /// </summary>
+        /// <exception cref="HttpException">Thrown if response status code is not 200 OK</exception>
+        public static string GetContent(this HttpResponseMessage httpResponse)
+        {
+            if (httpResponse.StatusCode  == HttpStatusCode.OK)
+            {
+                return httpResponse.Content.ReadAsStringAsync().Result;
+            }
+            throw new HttpException(Resource.HttpStatusCodeNotOk);
+        }
+    }
+}
