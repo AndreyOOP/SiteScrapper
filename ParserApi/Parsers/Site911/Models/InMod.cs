cdb88da3b42d521dbca86a6e0c3c1b88fd86e392
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-03 22:06:32 +0300

    PI-2 | Implement First Parsing Step  - search request by part id

---
+++ ParserApi/Parsers/Site911/Models/InMod.cs
@@ -0,0 +1,7 @@
+﻿namespace ParserApi.Parsers.Site911.Models
+{
+    public class InMod
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ ParserApi/Parsers/Site911/WorkUnits/InToHtmlStep1.cs
@@ -0,0 +1,27 @@
+﻿using ParserApi.Extensions;
+using ParserApi.Parsers.Site911.Models;
+using System;
+using System.Net.Http;
+
+namespace ParserApi.Parsers.Site911.WorkUnits
+{
+    public class InToHtmlStep1 : Site911WorkerBase<InMod, HtmlStep1>
+    {
+        private readonly HttpClient httpClient;
+
+        public InToHtmlStep1(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public override HtmlStep1 Parse(InMod model)
+        {
+            var searchUri = new Uri(Site911Base, $"/search/{model.Id}");
+            var response = httpClient.GetAsync(searchUri).Result;
+            return new HtmlStep1
+            {
+                Html = response.GetContent()
+            };
+        }
+    }
+}
