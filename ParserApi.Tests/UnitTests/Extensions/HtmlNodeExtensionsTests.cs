8b5beb320b6ac4e218f56d76dc14631e98429301
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-05 15:24:50 +0300

    PI-2 | Add HtmlNodeExtensions & Unit Tests

---
+++ ParserApi.Tests/UnitTests/Extensions/HtmlNodeExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParserApi.Exceptions;
+using ParserApi.Extensions;
+
+namespace ParserApi.Tests.UnitTests.Extensions
+{
+    [TestClass]
+    public class HtmlNodeExtensionsTests
+    {
+        HtmlDocument htmlDocument;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            htmlDocument = new HtmlDocument();
+        }
+
+        [TestMethod]
+        [DataRow(" - ", false)]
+        [DataRow("/style/2/minus.gif", false)]
+        [DataRow("/style/2/plus.gif", true)]
+        public void GetSign_CorrectAttribute_AsExpected(string val, bool expected)
+        {
+            htmlDocument.LoadHtml($"<th><img title='Нет в наличии' alt='нет' src='{val}' /></th>");
+            var testNode = htmlDocument.DocumentNode.FirstChild;
+
+            Assert.AreEqual(expected, testNode.GetSign());
+        }
+
+        [TestMethod]
+        [DataRow("src=''")]
+        [DataRow("")]
+        public void GetSign_MissingOrIncorrectSrc_ParsingException(string val)
+        {
+            htmlDocument.LoadHtml($"<th><img title='Нет в наличии' alt='нет' {val} /></th>");
+            var testNode = htmlDocument.DocumentNode.FirstChild;
+
+            Assert.ThrowsException<ParsingException>(() => testNode.GetSign());
+        }
+    }
+}
