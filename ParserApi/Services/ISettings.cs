7b1d97b8aa2a15c639112ae7e24881cd8212ba27
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-06 23:28:37 +0300

    PI-5 | Add ILoggerSettingsProvider & ISettings Interface  - add implementation - LoggerSettingsProvider

---
+++ ParserApi/Services/ISettings.cs
@@ -0,0 +1,16 @@
+﻿namespace ParserApi.Services
+{
+    public interface ISettings<TSettings>
+    {
+        /// <summary>
+        /// Current settings
+        /// </summary>
+        TSettings Settings { get; }
+
+        /// <summary>
+        /// Update setting object to new one
+        /// </summary>
+        /// <param name="settings">New settings</param>
+        void Update(TSettings settings);
+    }
+}
---
+++ ParserApi/Services/LoggerSettingsProvider.cs
@@ -0,0 +1,32 @@
+﻿using ParserApi.Controllers.Models;
+using ParserApi.Parsers.Site911.Models;
+using ParserCore.Abstraction;
+
+namespace ParserApi.Services
+{
+    public class LoggerSettingsProvider : ILoggerSettingsProvider
+    {
+        public WorkerLogSettings Settings { get; private set; } = new ExceptionLoggerSettings();
+
+        public void Update(WorkerLogSettings settings)
+        {
+            Settings = settings;
+        }
+
+        public void Update(InMemoryLoggerLevel loggerLevel)
+        {
+            switch (loggerLevel)
+            {
+                case InMemoryLoggerLevel.VerboseLogger:
+                    Settings = new VerboseLoggerSettings();
+                    break;
+                case InMemoryLoggerLevel.VerboseParseLoggerSettings:
+                    Settings = new VerboseParseLoggerSettings();
+                    break;
+                case InMemoryLoggerLevel.ExceptionLoggerSettings:
+                    Settings = new ExceptionLoggerSettings();
+                    break;
+            }
+        }
+    }
+}
