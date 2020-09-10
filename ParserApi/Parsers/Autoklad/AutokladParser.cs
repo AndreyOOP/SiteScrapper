936e835461d86b91c5ceccc93a117996b98fd02c
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-10 22:10:49 +0300

    PI-6 | Add AutokladParser  - add models

---
+++ ParserApi/Parsers/Autoklad/AutokladParser.cs
@@ -0,0 +1,21 @@
+﻿using ParserApi.Parsers.Autoklad.Models;
+using ParserCore;
+using ParserCore.Extensions;
+
+namespace ParserApi.Parsers.Autoklad
+{
+    public class AutokladParser : ParserBase<InAK, ResultAK>
+    {
+        public AutokladParser(IWorkersContainer workersContainer) : base(workersContainer)
+        {
+        }
+
+        protected override ResultAK PrepareResult()
+        {
+            return new ResultAK
+            {
+                HtmlS1 = result.Get<HtmlS1AK>()
+            };
+        }
+    }
+}
---
+++ ParserApi/Parsers/Autoklad/Models/ResultAK.cs
@@ -0,0 +1,8 @@
+﻿namespace ParserApi.Parsers.Autoklad.Models
+{
+    public class ResultAK
+    {
+        public object HtmlS1 { get; set; }
+        public object Log { get; set; }
+    }
+}
