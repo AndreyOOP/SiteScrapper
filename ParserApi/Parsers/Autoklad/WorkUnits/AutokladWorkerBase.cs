0a41ebaa4349cfaa963f09d2d32f4308efded419
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-10 22:11:46 +0300

    PI-6 | Add AutokladWorkerBase

---
+++ ParserApi/Parsers/Autoklad/WorkUnits/AutokladWorkerBase.cs
@@ -0,0 +1,14 @@
+﻿using ParserCore;
+using System;
+
+namespace ParserApi.Parsers.Autoklad.WorkUnits
+{
+    public abstract class AutokladWorkerBase<TIn, TOut> : IWorker<TIn, TOut>
+    {
+        protected readonly Uri AutokladBase = new Uri("https://www.autoklad.ua/");
+
+        public virtual bool IsExecutable(TIn model) => true;
+
+        public abstract TOut Parse(TIn model);
+    }
+}
