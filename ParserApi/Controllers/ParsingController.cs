56d3dc2356fd3e241530a23828c81a59cb036833
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-10 22:18:38 +0300

    PI-6 | Add Autoklad Parser to ParsingController  - controller has to be refactored - currently it just demonstrate that solution works

--- ParserApi/Controllers/ParsingController.cs
+++ ParserApi/Controllers/ParsingController.cs
@@ -1,4 +1,6 @@
 ﻿using ParserApi.Controllers.Models;
+using ParserApi.Parsers.Autoklad;
+using ParserApi.Parsers.Autoklad.Models;
 using ParserApi.Parsers.Site911.Models;
 using ParserApi.Parsers.Site911ParserCore;
 using ParserCore.Abstraction;
@@ -10,12 +12,17 @@ namespace ParserApi.Controllers
     public class ParsingController : ApiController
     {
         private Site911Parser site911Parser;
-        private IInMemoryWorkerLogger memoryLogger;
+        private AutokladParser autokladParser;
+        private IInMemoryWorkerLogger memoryLogger911;
+        private IInMemoryWorkerLogger memoryLoggerAK;
 
-        public ParsingController(IInMemoryWorkerLogger memoryLogger, Site911Parser site911Parser)
+        public ParsingController(IInMemoryWorkerLogger memoryLogger911, IInMemoryWorkerLogger memoryLoggerAK, Site911Parser site911Parser, AutokladParser autokladParser)
         {
             this.site911Parser = site911Parser;
-            this.memoryLogger = memoryLogger;
+            this.autokladParser = autokladParser;
+
+            this.memoryLogger911 = memoryLogger911;
+            this.memoryLoggerAK = memoryLoggerAK;
 
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
         }
@@ -24,18 +31,38 @@ namespace ParserApi.Controllers
         [Route("api/parse/{sparePartId}")]
         public object ParseSingleModel([FromUri]string sparePartId, [FromBody]RequestParams @params)
         {
-            var result = new Result();
+            var result911 = new Result();
+            var resultAK = new ResultAK();
 
             try
             {
-                result = site911Parser.Parse(new In { Id = sparePartId });
+                result911 = site911Parser.Parse(new In { Id = sparePartId });
             }
             finally
             {
-                result.Log = @params.ShowLog ? memoryLogger.Records : memoryLogger.ErrorRecords;
+                result911.Log = @params.ShowLog ? memoryLogger911.Records : memoryLogger911.ErrorRecords;
             }
 
-            return result;
+            try
+            {
+                resultAK = autokladParser.Parse(new InAK { Id = sparePartId });
+            }
+            finally
+            {
+                resultAK.Log = @params.ShowLog ? memoryLoggerAK.Records : memoryLoggerAK.ErrorRecords;
+            }
+
+            return new Summary
+            {
+                Site911Result = result911,
+                AutokladResult = resultAK
+            };
+        }
+
+        public class Summary
+        {
+            public Result Site911Result { get; set; }
+            public ResultAK AutokladResult { get; set; }
         }
     }
 }
