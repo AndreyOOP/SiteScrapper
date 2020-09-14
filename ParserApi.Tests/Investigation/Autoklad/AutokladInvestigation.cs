cd4527448b7334bab058c3e2ae51846c6af86543
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-14 22:24:31 +0300

    PI-6 | Add Investigation Tests & Data Samples

---
+++ ParserApi.Tests/Investigation/Autoklad/AutokladInvestigation.cs
@@ -0,0 +1,36 @@
+﻿using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParserApi.Parsers.Autoklad.Models;
+using ParserApi.Parsers.Autoklad.WorkUnits;
+using System.IO;
+using System.Net.Http;
+
+namespace ParserApi.Tests.Investigation.Autoklad
+{
+    [TestClass]
+    public class AutokladInvestigation
+    {
+        [TestMethod]
+        public void GetDataFromWebRequest()
+        {
+            var step1 = new InToHtmlS1AK(new HttpClient());
+            var http = step1.Parse(new InAK { Id = "MR984375" });
+
+            var step2 = new HtmlToFirstResultAK();
+            var result = step2.Parse(http);
+        }
+
+        [TestMethod]
+        public void GetDataFromFile()
+        {
+            var htmlDocument = new HtmlDocument();
+            var html = File.ReadAllText(@"Investigation\Autoklad\HtmlStep1.html");
+            htmlDocument.LoadHtml(html);
+
+            var step2 = new HtmlToFirstResultAK();
+            var result = step2.Parse(new HtmlS1AK { Html = html });
+
+            Assert.AreEqual(4, result.FirstResult.Count);
+        }
+    }
+}
