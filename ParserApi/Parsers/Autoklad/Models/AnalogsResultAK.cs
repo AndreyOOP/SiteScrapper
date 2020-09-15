bc9859615ca3bac0051052de433cf695abea215e
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-15 21:58:18 +0300

    PI-6 | Add HtmlToSecondResultAK

---
+++ ParserApi/Parsers/Autoklad/Models/AnalogsResultAK.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ParserApi.Parsers.Autoklad.Models
+{
+    public class SecondResultAK
+    {
+        public Dictionary<string, List<RowResultAK>> Analogs { get; set; }
+        public Dictionary<string, List<RowResultAK>> Same { get; set; }
+    }
+
+    public class RowResultAK
+    {
+        public string Name { get; set; }
+        public string Period { get; set; }
+        public string Price { get; set; }
+    }
+}
