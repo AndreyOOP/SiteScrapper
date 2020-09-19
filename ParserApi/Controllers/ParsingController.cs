688f4c54f3cc2c2c9fc6d5cbf236eb134221e721
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-19 17:34:34 +0300

    FE-0 | Add ParsersResult Method  - will be moved to separate service)

--- ParserApi/Controllers/ParsingController.cs
+++ ParserApi/Controllers/ParsingController.cs
@@ -1,11 +1,14 @@
 ﻿using ParserApi.Controllers.Models;
+using ParserApi.Parsers;
 using ParserApi.Parsers.Autoklad;
 using ParserApi.Parsers.Autoklad.Models;
 using ParserApi.Parsers.Site911.Models;
 using ParserApi.Parsers.Site911ParserCore;
 using ParserCore;
+using System.Collections.Generic;
 using System.Net;
 using System.Web.Http;
+using System.Linq;
 
 namespace ParserApi.Controllers
 {
@@ -32,10 +35,40 @@ namespace ParserApi.Controllers
             result911.Log = @params.ShowLog ? site911Parser.WorkerLogger.Records : site911Parser.WorkerLogger.ErrorRecords;
             resultAK.Log = @params.ShowLog ? autokladParser.WorkerLogger.Records : autokladParser.WorkerLogger.ErrorRecords;
 
+            var result = Create(result911, resultAK, sparePartId);
+            result.Site911Log = result911.Log;
+            result.AutokladLog = resultAK.Log;
+            return result;
+        }
+
+        // ToDo: move to service
+        private ParsersResult Create(Result911 result911, ResultAK resultAK, string partId)
+        {
+            var mainTable = new List<MainPriceTableRow>();
+            mainTable.Add(new MainPriceTableRow 
+            { 
+                ParserName = nameof(Parser.Site911),
+                Name = result911?.Primary?.Name,
+                Brand = "", // todo
+                Price = result911?.Primary?.Price.ToString(),
+                LinkToSource = $"https://911auto.com.ua/search/{partId}"
+            });
+            var resAK = resultAK.FirstResult?.FirstResult?.Select(r => new MainPriceTableRow
+            {
+                ParserName = nameof(Parser.Autoklad),
+                Name = r?.Name,
+                Brand = r?.Brand,
+                Price = r?.Price,
+                LinkToSource = $"https://www.autoklad.ua{r?.PartBrandLink}"
+            });
+            if(resAK != null)
+                mainTable.AddRange(resAK);
+
+            // ToDo: same + analogs + for request
+
             return new ParsersResult
             {
-                Site911 = result911,
-                Autoklad = resultAK
+                Main = mainTable
             };
         }
 
