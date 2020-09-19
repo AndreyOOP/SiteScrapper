bb402476ede4908e2524b44059706f22099a1748
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-19 17:38:16 +0300

    FE-0 | Add First Version of Result Formatted as Table  - to be refactored

--- ParserApi/FE/js/scripts.js
+++ ParserApi/FE/js/scripts.js
@@ -1,6 +1,6 @@
-﻿//var hostApi = "https://localhost:44331";
+﻿var hostApi = "https://localhost:44331";
 //var hostApi = "https://localhost:1234";
-var hostApi = "https://f2e78241c2fe.ngrok.io";
+//var hostApi = "https://f2e78241c2fe.ngrok.io";
 
 async function parse(id) {
 
@@ -15,10 +15,32 @@ async function parse(id) {
 
     if (response.ok) {
         let txtResponse = await response.text();
+
         insertHtm("#search_result", txtResponse);
+        insertHtm("#tbl_upd", createTable(txtResponse));
     } else {
         console.log(response.status);
     }
+}
+
+// ToDo: refactor => createTable(name, rows)
+function createTable(jsonText) {
+
+    var json = JSON.parse(jsonText);
+
+    var table = "";
+    for (var i = 0; i < json.Main.length; i++) {
+        var x = json.Main[i];
+        var row = "<tr>" +
+            "<td>" + x.ParserName + "</td>" +
+            "<td>" + x.Brand + "</td>" +
+            "<td>" + x.Name + "</td>" +
+            "<td>" + x.Price + "</td>" +
+            "<td>" + "<a href='" + x.LinkToSource + "'>Link</a>" + "</td>" +
+            "</tr>";
+        table += row;
+    }
+    return table;
 }
 
 function insertHtm(selector, html) {
