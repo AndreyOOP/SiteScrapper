9a6d522cd0e6edbd4e010c059380aa61e5ce99cf
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-19 16:59:48 +0300

    FE-0 | Update ParsersResult  - generalize result of multiple parsers

--- ParserApi/Controllers/Models/ParsersResult.cs
+++ ParserApi/Controllers/Models/ParsersResult.cs
@@ -1,11 +1,52 @@
-﻿using ParserApi.Parsers.Autoklad.Models;
-using ParserApi.Parsers.Site911.Models;
+﻿using ParserApi.Parsers;
+using System;
+using System.Collections.Generic;
 
 namespace ParserApi.Controllers.Models
 {
     public class ParsersResult
     {
-        public Result911 Site911 { get; set; }
-        public ResultAK Autoklad { get; set; }
+        public IEnumerable<MainPriceTableRow> Main { get; set; }
+        public IEnumerable<PerRequestTableRow> PerRequest { get; set; }
+        public IEnumerable<AnalogTableRow> Analogs { get; set; }
+        public IEnumerable<SameTableRow> SameIds { get; set; }
+        public object Site911Log { get; set; }
+        public object AutokladLog { get; set; }
+    }
+
+    public class MainPriceTableRow
+    {
+        public string ParserName { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string LinkToSource { get; set; }
+    }
+
+    public class PerRequestTableRow
+    {
+        public Parser ParserName { get; set; }
+        public string PartId { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public int Qty { get; set; }
+        public double PriceUah { get; set; }
+        public Uri LinkToSource { get; set; }
+    }
+
+    public class AnalogTableRow
+    {
+        public Parser ParserName { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public Uri LinkToSource { get; set; }
+    }
+
+    public class SameTableRow
+    {
+        public Parser ParserName { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public Uri LinkToSource { get; set; }
     }
 }
--- ParserApi/Parsers/Autoklad/Models/ResultAK.cs
+++ ParserApi/Parsers/Autoklad/Models/ResultAK.cs
@@ -2,7 +2,7 @@
 {
     public class ResultAK
     {
-        public object FirstResult { get; set; }
+        public FirstResultStoreAK FirstResult { get; set; }
         public object AnalogResult { get; set; }
         public object Log { get; set; }
     }
