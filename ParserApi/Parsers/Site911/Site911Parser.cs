f081b8ee851a0e86f24fbf8c44b6a619d194bba5
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-22 21:11:07 +0300

    FE-0 | Update Result Creation for Site911

--- ParserApi/Parsers/Site911/Site911Parser.cs
+++ ParserApi/Parsers/Site911/Site911Parser.cs
@@ -1,7 +1,11 @@
-﻿using ParserApi.Parsers.Site911.Models;
+﻿using ParserApi.Controllers.Models;
+using ParserApi.Parsers.Site911.Models;
 using ParserCore;
 using ParserCore.Abstraction;
 using ParserCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Unity;
 
 namespace ParserApi.Parsers.Site911ParserCore
@@ -22,9 +26,49 @@ namespace ParserApi.Parsers.Site911ParserCore
         {
             return new Result911
             {
-                Primary = result.Get<PrimaryResultStep2>(),
-                Secondary = result.Get<SecondaryResultStep4>()
+                Parts = GetParts(),
+                PerRequestParts = GetPerRequestParts(),
+                AnalogParts = new List<AnalogPart>(),
+                MatchingParts = new List<MatchingPart>(),
+                RawData = new RawDataSite911
+                {
+                    Primary = result.Get<PrimaryResultStep2>(),
+                    Secondary = result.Get<SecondaryResultStep4>()
+                }
             };
+        }
+
+        private IEnumerable<Part> GetParts()
+        {
+            var primary = result.Get<PrimaryResultStep2>();
+            var table = new List<Part>
+            {
+                new Part
+                {
+                    ParserName = nameof(Parser.Site911),
+                    Name = primary?.Name,
+                    Brand = "", // todo
+                    Price = primary?.Price.ToString(),
+                    LinkToSource = $"https://911auto.com.ua/search/" // todo
+                }
+            };
+            return table;
+        }
+        
+        private IEnumerable<PerRequestPart> GetPerRequestParts()
+        {
+            var secondary = result.Get<SecondaryResultStep4>();
+            var perRequestParts = secondary.Table?.Select(r => new PerRequestPart
+            {
+                ParserName = nameof(Parser.Site911),
+                PartId = r.PartId,
+                Brand = r.Brand,
+                Name = r.Name,
+                Qty = r.Qty,
+                PriceUah = r.PriceUah,
+                LinkToSource = new Uri($"https://911auto.com.ua/search/{r.PartId}")
+            });
+            return perRequestParts;
         }
     }
 }
