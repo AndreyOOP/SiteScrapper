8732f8845809af4f1cf309cbab4c815e6a394fd5
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-24 23:45:13 +0300

    PC-10 | Add HtmlNodeProvider Unit Tests  - add HtmlAgilityPack reference to test project

---
+++ ParserCoreTests/UnitTests/HtmlNodeProviderTests.cs
@@ -0,0 +1,39 @@
+﻿using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParserCore.Models.HtmlNodeProviders;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class HtmlNodeProviderTests
+    {
+        [TestMethod]
+        public void HtmlNodeFromNodeProvider_CreateProvider_HtmlNode()
+        {
+            var html = "<head></head>";
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var nodeProvider = new HtmlNodeFromNodeProvider(htmlDocument.DocumentNode);
+
+            Assert.AreEqual(typeof(HtmlNode), nodeProvider.Node.GetType());
+        }
+
+        [TestMethod]
+        public void HtmlNodeFromHtmlProvider_CreateProvider_HtmlNode()
+        {
+            var nodeProvider = new HtmlNodeFromHtmlProvider("<head></head>");
+
+            Assert.AreEqual(typeof(HtmlNode), nodeProvider.Node.GetType());
+        }
+
+        [TestMethod]
+        public void HtmlNodeFromHtmlProvider_IncorrectHtml_()
+        {
+            var nodeProvider = new HtmlNodeFromHtmlProvider("not html");
+
+            Assert.AreEqual(typeof(HtmlNode), nodeProvider.Node.GetType());
+        }
+    }
+
+}
