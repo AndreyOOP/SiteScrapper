f1b919aeed94b8cd2ef4585969aeef4137de4f7f
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-26 01:55:34 +0300

    PC-10 | Add AttributeExtensions GetConstructorArgument  - get value of constructor argument of type T at position

--- ParserCore/Extensions/AttributeExtensions.cs
+++ ParserCore/Extensions/AttributeExtensions.cs
@@ -13,5 +13,16 @@ namespace ParserCore.Extensions
         /// <returns>Custom attribute data</returns>
         public static CustomAttributeData GetCustomAttributeData<T>(this PropertyInfo propertyInfo)
             => propertyInfo.CustomAttributes.FirstOrDefault(attribute => attribute.AttributeType == typeof(T));
+
+        // ToDo: get argument by type (if single) if not use position 
+        /// <summary>
+        /// Get value of constructor argument of type <typeparam name="T" /> at <param name="position" />
+        /// </summary>
+        /// <typeparam name="T">Type of the constructor argument</typeparam>
+        /// <param name="data">Custom attribute metadata</param>
+        /// <param name="position">Optional, position of the constructor argument</param>
+        /// <returns>Value of the constructor argument</returns>
+        public static T GetConstructorArgument<T>(this CustomAttributeData data, int position = 0)
+            => (T)data.ConstructorArguments[position].Value;
     }
 }
