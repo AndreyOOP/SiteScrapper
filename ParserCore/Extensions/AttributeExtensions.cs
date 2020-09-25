031a65d24615ff6237e87d29ac1d080fa83df58c
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-26 00:56:12 +0300

    PC-10 | Add AttributeExtensions GetCustomAttributeData  - get custom attribute of type T from PropertyInfo

---
+++ ParserCore/Extensions/AttributeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ParserCore.Extensions
+{
+    public static class AttributeExtensions
+    {
+        /// <summary>
+        /// Get custom attribute from <param name="propertyInfo" /> of type <typeparam name="T" />. Returns default if could not found the attribute 
+        /// </summary>
+        /// <typeparam name="T">Custom attribute type</typeparam>
+        /// <param name="propertyInfo">Property metadata</param>
+        /// <returns>Custom attribute data</returns>
+        public static CustomAttributeData GetCustomAttributeData<T>(this PropertyInfo propertyInfo)
+            => propertyInfo.CustomAttributes.FirstOrDefault(attribute => attribute.AttributeType == typeof(T));
+    }
+}
