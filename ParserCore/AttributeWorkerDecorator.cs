8b02726fb72ceba13d6c523fdb83cc1057068607
AndreyOOP <andrew_yudaichev@i.ua>
2020-09-28 22:28:23 +0300

    PC-10 | Add AttributeWorkerDecorator  - AttributeWorkerDecorator required to add values to properties marked via XPath attribute  - add unit tests

---
+++ ParserCore/AttributeWorkerDecorator.cs
@@ -0,0 +1,45 @@
+﻿using ParserCore.Abstraction;
+using ParserCore.Attributes;
+using ParserCore.Extensions;
+
+namespace ParserCore
+{
+    public class AttributeWorkerDecorator<TIn, TOut> : IWorker<TIn, TOut>
+        where TIn : IHtmlNodeProvider
+        where TOut : new()
+    {
+        private IWorker<TIn, TOut> worker;
+
+        /// <summary>
+        /// TOut model produced by <param name="worker"> will be updated according to <see cref="XPathAttribute"/>
+        /// </summary>
+        /// <param name="worker">Worker to be decorated</param>
+        public AttributeWorkerDecorator(IWorker<TIn, TOut> worker)
+        {
+            this.worker = worker;
+        }
+
+        public bool IsExecutable(TIn model)
+            => worker.IsExecutable(model);
+
+        public TOut Parse(TIn model)
+        {
+            var htmlNode = model.Node;
+            var tOut = worker.Parse(model) ?? new TOut();
+
+            foreach(var property in tOut.GetType().GetProperties())
+            {
+                var xPath = property.GetCustomAttributeData<XPathAttribute>()
+                                    ?.GetConstructorArgument<string>();
+                if(xPath != null)
+                {
+                    var value = htmlNode.SelectSingleNode(xPath)?.InnerText;
+                    // ToDo: convert to property type
+                    property.SetValue(tOut, value);
+                }
+            }
+
+            return tOut;
+        }
+    }
+}
---
+++ ParserCoreTests/UnitTests/AttributeWorkerDecoratorTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParserCore;
+using ParserCore.Attributes;
+using ParserCore.Models.HtmlNodeProviders;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AttributeWorkerDecoratorTests
+    {
+        class TIn : HtmlNodeFromHtmlProvider
+        {
+            public TIn(string html) : base(html) { }
+        }
+
+        class XPathTestClass
+        {
+            [XPath("//div")]
+            public string FirstDivFromRoot { get; set; }
+
+            [XPath("//div[@class='container']")]
+            public string DivWithClassContainer { get; set; }
+
+            [XPath("//div[@class='not-existed-class']")]
+            public string NotFound { get; set; }
+
+            // if you need apply function to xPath result:
+            private string ink;
+            [XPath("//a")]
+            public string Ink
+            {
+                get => ink.Substring(1);
+                set => ink = value;
+            }
+
+            [XPath("./html/body/header/nav/div[last()]")]
+            public string DivInNav { get; set; }
+        }
+
+        class XPathTestWorker : IWorker<TIn, XPathTestClass>
+        {
+            public bool IsExecutable(TIn model) => true;
+
+            public XPathTestClass Parse(TIn model) => new XPathTestClass();
+        }
+
+        [TestMethod]
+        [DataRow(@"
+        <html>
+            <head>
+                <meta charset='utf-8'>
+            </head>
+            <body>
+                <header>
+                    <nav id='header-nav' class='navbar navbar-default'>
+                        <div class='container'>About Container</div>
+                        <div>
+                            <h1>Header</h1>
+                            <a href='http://link.com'>Link</a>
+                        </div>
+                    </nav>
+                </header>
+                <div id='#id_div'></div>
+            </body>
+        </html>")]
+        public void Parse_TypeWithXPathAttributes_ValuesAsExpected(string html)
+        {
+            var attributeWorkerDecorator = new AttributeWorkerDecorator<TIn, XPathTestClass>(new XPathTestWorker());
+
+            var result = attributeWorkerDecorator.Parse(new TIn(html));
+
+            Assert.AreEqual("About Container", result.FirstDivFromRoot);
+            Assert.AreEqual("About Container", result.DivWithClassContainer);
+            Assert.IsNull(result.NotFound);
+            Assert.AreEqual("ink", result.Ink);
+            StringAssert.Contains(result.DivInNav, "Header");
+        }
+    }
+
+}
